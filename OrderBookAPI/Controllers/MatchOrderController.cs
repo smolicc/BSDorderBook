9bf27702c4dc2b13724de8924f6c86e51dcbc3a7
Jack-PC <jakob.smolic1@student.um.si>
2024-12-05 01:42:24 +0100

    feat: added API

---
+++ OrderBookAPI/Controllers/MatchOrderController.cs
@@ -0,0 +1,25 @@
+﻿using CoreLibrary.Services;
+using CoreLibrary.Data;
+using CoreLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderBookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchOrderController : ControllerBase
+    {
+        [HttpPost("buy/{amount}")]
+        public IActionResult BuyOrder(decimal amount)
+        {
+            //update paths
+            var dr = new DataRepository("CryptoExchangesData.json", "OrderBooksData.json");
+
+            List<string> response = new MatchOrdersService().MatchOrders(dr.LoadCryptoExchanges(), dr.LoadOrderBooks(), true, amount);
+
+            //Temp response
+            return Ok(response[1]);
+        }
+    }
+}
