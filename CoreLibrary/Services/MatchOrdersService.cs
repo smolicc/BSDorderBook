215ec92391bf5d06bc3ae67ab61983d845fb9098
Jack-PC <jakob.smolic1@student.um.si>
2024-12-08 01:02:36 +0100

    fix: updated the service for better precision

--- CoreLibrary/Services/MatchOrdersService.cs
+++ CoreLibrary/Services/MatchOrdersService.cs
@@ -3,6 +3,7 @@ using CoreLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,29 +36,46 @@ namespace CoreLibrary.Services
                 {
                     if (remainingAmount == 0)
                     {
-                        result.FinalResponse = $"Total for {targetAmount} BTC is: {totalPrice} EUR";
+                        result.FinalResponse = $"Total for {targetAmount} BTC is: {Math.Round(totalPrice, 2)} EUR";
                         break;
                     }
 
                     CryptoExchange exchangeBalance = cryptoExchanges.First(b => b.Id == ask.Exchange);
 
-                    decimal maxBuyable = exchangeBalance.EURBalance / ask.Order.Price;
-                    decimal amountToBuy = Math.Min(Math.Min(maxBuyable, ask.Order.Amount), remainingAmount);
+                    BigInteger exBalanceEUR = new BigInteger(exchangeBalance.EURBalance * 100_000_000m);
+                    BigInteger OrderPrice = new BigInteger(ask.Order.Price * 100_000_000m);
 
-                    if (amountToBuy > 0)
+                    BigInteger OrderAmount = new BigInteger(ask.Order.Amount * 100_000_000m);
+                    BigInteger remainingAmountInSatoshis = new BigInteger(remainingAmount * 100_000_000m);
+
+                    BigInteger maxBuyable = (exBalanceEUR * 100_000_000) / OrderPrice;
+                    BigInteger amountToBuy = BigInteger.Min(BigInteger.Min(maxBuyable, OrderAmount), remainingAmountInSatoshis);
+
+                    if(amountToBuy > 0)
                     {
+                        decimal amountToBuyDecimal = (decimal)amountToBuy / 100_000_000m;
+
                         SortedOrder so = new()
                         {
                             Exchange = ask.Exchange,
                             Order = ask.Order
                         };
 
-                        so.Order.Amount = amountToBuy;
+                        so.Order.Amount = amountToBuyDecimal;
                         result.BestExecutionPlan.Add(so);
-                        
-                        exchangeBalance.EURBalance -= amountToBuy * ask.Order.Price;
-                        totalPrice += amountToBuy * ask.Order.Price;
-                        remainingAmount -= amountToBuy;
+
+                        exBalanceEUR -= (amountToBuy  * OrderPrice) / 100_000_000;
+                        totalPrice += (decimal)(amountToBuy * OrderPrice) / (100_000_000m * 100_000_000m);
+                        remainingAmountInSatoshis -= amountToBuy;
+
+                        exchangeBalance.EURBalance = Math.Round((decimal)exBalanceEUR / 100_000_000m, 2);
+                        remainingAmount = (decimal)remainingAmountInSatoshis / 100_000_000m;
+                    }
+
+                    if (cryptoExchanges.All(o => o.EURBalance == 0) && remainingAmount > 0)
+                    {
+                        result.FinalResponse = $"Crypto exchange EUR balance too low for remaining {remainingAmount} BTC";
+                        break;
                     }
                 }
             }
@@ -75,35 +93,49 @@ namespace CoreLibrary.Services
                 {
                     if (remainingAmount == 0)
                     {
-                        result.FinalResponse = $"Total for {targetAmount} BTC is: {totalPrice} EUR";
+                        result.FinalResponse = $"Total for {targetAmount} BTC is: {Math.Round(totalPrice, 2)} EUR";
                         break;
                     }
 
                     CryptoExchange exchangeBalance = cryptoExchanges.First(b => b.Id == bid.Exchange);
 
-                    decimal amountToSell = Math.Min(Math.Min(exchangeBalance.BTCBalance, bid.Order.Amount), remainingAmount);
+                    BigInteger exBalanceBTC = new BigInteger(exchangeBalance.BTCBalance * 100_000_000m);
+                    BigInteger orderAmount = new BigInteger(bid.Order.Amount * 100_000_000m);
+                    BigInteger orderPrice = new BigInteger(bid.Order.Price * 100_000_000m);
+                    BigInteger remainingAmountInSatoshis = new BigInteger(remainingAmount * 100_000_000m);
+
+                    BigInteger amountToSell = BigInteger.Min(BigInteger.Min(exBalanceBTC, orderAmount), remainingAmountInSatoshis);
 
                     if (amountToSell > 0)
                     {
+                        decimal amountToSellDecimal = (decimal)amountToSell / 100_000_000m;
+
                         SortedOrder so = new()
                         {
                             Exchange = bid.Exchange,
                             Order = bid.Order
                         };
 
-                        so.Order.Amount = amountToSell;
+                        so.Order.Amount = amountToSellDecimal;
                         result.BestExecutionPlan.Add(so);
 
-                        exchangeBalance.BTCBalance -= amountToSell;
-                        totalPrice += amountToSell * bid.Order.Price;
-                        remainingAmount -= amountToSell;
+                        exchangeBalance.BTCBalance -= amountToSellDecimal;
+                        totalPrice += (decimal)(amountToSell * orderPrice) / (100_000_000m * 100_000_000m);
+
+                        remainingAmount = (decimal)(remainingAmountInSatoshis - amountToSell) / 100_000_000m;
+                    }
+
+                    if (cryptoExchanges.All(o => o.BTCBalance == 0) && remainingAmount > 0)
+                    {
+                        result.FinalResponse = $"Crypto exchange BTC balance too low for remaining {remainingAmount} BTC";
+                        break;
                     }
                 }
             }
 
-            if (remainingAmount > 0)
+            if (remainingAmount > 0 && result.FinalResponse == "")
             {
-                result.FinalResponse = $"Crypto exchange balance too low for remaining {remainingAmount} BTC";
+                result.FinalResponse = "Out of orders";
             }
 
             return result;
